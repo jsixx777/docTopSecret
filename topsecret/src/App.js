42bdb4687a312d13e81a0cb21692a977eb3dfbdd
JPSiler <jsixx77@hotmail.com>
2021-10-26 12:34:02 -0500

    basic react setup

---
+++ topsecret/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+
+export default function App () {
+  return (
+    <div className="main">
+      <h3 className="title">Yo React</h3>
+    </div>
+  )
+}
